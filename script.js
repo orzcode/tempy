9d329bd5b5ce838be4539c505e19d523659f17f9
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-01 20:33:34 +1300

    api

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const api = () => {
-
-	
-}
+  const apiURL = "http://api.weatherapi.com/v1/current.json?key=158e47f266244b039b211441241801&q=Christchurch&aqi=no";
+	fetch(apiURL, {mode: 'cors'})
+    .then(function (response) {
+		console.log(response.json());
+    })
+    .catch(function (err) {
+      // Error :(
+    });
+};
+api()
