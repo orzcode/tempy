57eae437a2c55a92842d272aa9426c30d6ce08cf
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-02 20:30:27 +1300

    better api with location param

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const api = () => {
-  const apiURL = "http://api.weatherapi.com/v1/current.json?key=158e47f266244b039b211441241801&q=Christchurch&aqi=no";
+const api = (location) => {
+  const key64 = "MTU4ZTQ3ZjI2NjI0NGIwMzliMjExNDQxMjQxODAx";
+  const key = atob(key64);
+  const apiURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location}&aqi=no`;
 	fetch(apiURL, {mode: 'cors'})
     .then(function (response) {
 		console.log(response.json());
+    return response.json()
     })
     .catch(function (err) {
-      // Error :(
+      //console.log("Error: " + err)
+      return err;
     });
 };
-api()
+api("christchurch")
