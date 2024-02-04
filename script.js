53478e681064f0bac1b9c743a2b9624f51564122
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-04 16:53:14 +1300

    need to set location updater

--- script.js
+++ script.js
@@ -1,67 +1,94 @@
-const api = async (location) => {
-  try {
-    const key64 = "MTU4ZTQ3ZjI2NjI0NGIwMzliMjExNDQxMjQxODAx";
-    const key = atob(key64);
-    const apiURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location}&aqi=no`;
+const props = () => {
+  const key64 = "MTU4ZTQ3ZjI2NjI0NGIwMzliMjExNDQxMjQxODAx";
+  const key = atob(key64);
 
-    const response = await fetch(apiURL, { mode: 'cors' });
-    const data = await response.json();
+  const _location = "christchurch"; // Make sure to set a default location
 
-    //console.log(data);
-    dataProcessor(data);
+  const realTimeDataURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${_location}`;
+  const hourlyDataURL = `http://api.weatherapi.com/v1/hourly.json?key=${key}&q=${_location}`;
 
-    return data;
-  } catch (error) {
-    //console.error("Error: ", error);
-    //throw error;
-  }
+  return {
+    get key() {
+      return key;
+    },
+    get location() {
+      return _location;
+    },
+    set location(newLocation) {
+      _location = newLocation;
+    },
+    get realTimeDataURL() {
+      return realTimeDataURL;
+    },
+    get hourlyDataURL() {
+      return hourlyDataURL;
+    },
+  };
 };
-api("christchurch")
+
+props().location = "testlocation";
+console.log(props().location);
+
+const apiMgr = () => {
+  const { key, location, realTimeDataURL, hourlyDataURL } = props();
+
+  const apiFetcher = async (queryType) => {
+    try {
+      const apiURL = queryType;
+
+      const response = await fetch(apiURL, { mode: "cors" });
+      const data = await response.json();
+
+      dataProcessor(data);
+      //sends Object to be pruned
+
+      return data;
+    } catch (error) {
+      //console.error("Error: ", error);
+      //throw error;
+    }
+  };
+
+  const getData = (queryType) => {
+    switch(queryType) {
+      case "current":
+        apiFetcher(realTimeDataURL);
+        break;
+      case "hourly":
+        apiFetcher(hourlyDataURL);
+        break;
+      case "future":
+        apiFetcher(futureDataURL);
+        break;
+    }
+  }
+
+  return { getData };
+};
+
+apiMgr().getData("current")
+
 
 
 const dataProcessor = (returnedJson) => {
   const newObj = {
-    "location": {
-        "name": returnedJson.location.name,
-        "country": returnedJson.location.country,
-        "localtime": returnedJson.location.localtime
+    location: {
+      name: returnedJson.location.name,
+      country: returnedJson.location.country,
+      localtime: returnedJson.location.localtime,
     },
-    "current": {
-        "temp_c": returnedJson.current.temp_c,
-        "is_day": returnedJson.current.is_day,
-        "wind_dir": returnedJson.current.wind_dir,
-        "precip_mm": returnedJson.current.precip_mm,
-        "condition": {
-            "text": returnedJson.current.condition.text,
-            "icon": returnedJson.current.condition.icon,
-            "code": returnedJson.current.condition.code
-        }
-    }
-}
-console.log(newObj)
-return newObj
-//   initialObj = {
-//     "location": {
-//         "name": "Christchurch",
-//         "region": "",
-//         "country": "New Zealand",
-//         "lat": -43.53,
-//         "lon": 172.63,
-//         "tz_id": "Pacific/Auckland",
-//         "localtime_epoch": 1706990695,
-//         "localtime": "2024-02-04 9:04"
-//     },
-//     "current": {
-//         "temp_c": 12,
-//         "is_day": 1,
-//         "condition": {
-//             "text": "Sunny",
-//             "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
-//             "code": 1000
-//         },
-//         "wind_dir": "ENE",
-//         "precip_mm": 0
-//     }
-// }
-}
-
+    current: {
+      temp_c: returnedJson.current.temp_c,
+      is_day: returnedJson.current.is_day,
+      wind_dir: returnedJson.current.wind_dir,
+      precip_mm: returnedJson.current.precip_mm,
+      condition: {
+        text: returnedJson.current.condition.text,
+        icon: returnedJson.current.condition.icon,
+        code: returnedJson.current.condition.code,
+      },
+    },
+  };
+  console.log(newObj);
+  return newObj;
+};
