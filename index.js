086915e302884c849aca2f08f1c3795a076dd57d
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-07 17:27:37 +1300

    cleanup dataprocessor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import storage from "./storage.js";
 
 const apiMgr = () => {
-  const { realTimeDataURL, hourlyDataURL } = storage.props();
+  const forecastURL = storage.props().forecastURL;
 
   const apiFetcher = async (queryType) => {
     try {
@@ -21,43 +21,87 @@ const apiMgr = () => {
     }
   };
 
-  const getData = (queryType) => {
-    switch(queryType) {
-      case "current":
-        apiFetcher(realTimeDataURL());
-        break;
-      case "hourly":
-        apiFetcher(hourlyDataURL());
-        break;
-    }
+  const getData = () => {
+    const data = apiFetcher(forecastURL());
+   dataProcessor(data);
+   //init error
   }
 
   return { getData };
 };
 
-apiMgr().getData("hourly")
+apiMgr().getData()
+
+
+//We want:
+//HOURLY 12/3/6/9 temp/precip/conditionImg (for a given day) 
+//and
+//date, temp (curr. and/or avg.), high, condition, condition image/code,
+//rain chance, wind dir. (for a given day)
 
 
 
 const dataProcessor = (returnedJson) => {
-  const newObj = {
-    location: {
-      name: returnedJson.location.name,
-      country: returnedJson.location.country,
-      localtime: returnedJson.location.localtime,
+  // hour should be 0-24
+  // day should be 0 or 1
+
+  const location = {
+    name: returnedJson.location.name,
+    country: returnedJson.location.country,
+    localtime: returnedJson.location.localtime,
+  };
+
+  const getHourlyData = (day, hour) => ({
+    temp_c: returnedJson.forecast.forecastday[day].hour[hour].temp_c,
+    is_day: returnedJson.forecast.forecastday[day].hour[hour].is_day,
+    wind_dir: returnedJson.forecast.forecastday[day].hour[hour].wind_dir,
+    rainchance: returnedJson.forecast.forecastday[day].hour[hour].chance_of_rain,
+    condition: {
+      text: returnedJson.forecast.forecastday[day].hour[hour].condition.text,
+      icon: returnedJson.forecast.forecastday[day].hour[hour].condition.icon,
+      code: returnedJson.forecast.forecastday[day].hour[hour].condition.code,
     },
-    current: {
-      temp_c: returnedJson.current.temp_c,
-      is_day: returnedJson.current.is_day,
-      wind_dir: returnedJson.current.wind_dir,
-      precip_mm: returnedJson.current.precip_mm,
-      condition: {
-        text: returnedJson.current.condition.text,
-        icon: returnedJson.current.condition.icon,
-        code: returnedJson.current.condition.code,
-      },
+  });
+
+  const today = {
+    temp_c: returnedJson.current.temp_c,
+    is_day: returnedJson.current.is_day,
+    wind_dir: returnedJson.current.wind_dir,
+    rainchance: returnedJson.forecast.forecastday[0].day.daily_chance_of_rain,
+    condition: {
+      text: returnedJson.current.condition.text,
+      icon: returnedJson.current.condition.icon,
+      code: returnedJson.current.condition.code,
+    },
+    hourly: {
+      12: getHourlyData(0, 12),
+      3: getHourlyData(0, 3),
+      6: getHourlyData(0, 6),
+      9: getHourlyData(0, 9),
     },
   };
-  console.log(newObj);
-  return newObj;
+
+  const tomorrow = {
+    temp_c: returnedJson.forecast.forecastday[1].day.maxtemp_c,
+    wind_dir: returnedJson.forecast.forecastday[1].day.wind_dir,
+    rainchance: returnedJson.forecast.forecastday[1].day.daily_chance_of_rain,
+    condition: {
+      text: returnedJson.forecast.forecastday[1].day.condition.text,
+      icon: returnedJson.forecast.forecastday[1].day.condition.icon,
+      code: returnedJson.forecast.forecastday[1].day.condition.code,
+    },
+    hourly: {
+      12: getHourlyData(1, 12),
+      3: getHourlyData(1, 3),
+      6: getHourlyData(1, 6),
+      9: getHourlyData(1, 9),
+    },
+  };
+
+  console.log(today)
+  return {
+    location,
+    today,
+    tomorrow,
+  };
 };
--- storage.js
+++ storage.js
@@ -34,17 +34,17 @@ const storage = {
     // storage.props().location("place") to set
     // storage.props().location() to get
 
-    const _realTimeDataURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location()}`;
-    const _hourlyDataURL = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location()}&days=2`;
+    //const _realTimeDataURL = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${location()}`;
+    const _forecastURL = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location()}&days=2`;
 
-    const realTimeDataURL = () => {
-      return _realTimeDataURL;
-    }
-    const hourlyDataURL = () => {
-      return _hourlyDataURL;
+    // const realTimeDataURL = () => {
+    //   return _realTimeDataURL;
+    // }
+    const forecastURL = () => {
+      return _forecastURL;
     }
 
-    return { location, realTimeDataURL, hourlyDataURL };
+    return { location, forecastURL };
 
   }
 
