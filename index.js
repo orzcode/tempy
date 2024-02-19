e7216a234cd34e73f0730e15f49dbd36401df795
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-20 10:51:36 +1300

    test min-height

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import apiMgr from "./apiMgr.js";
 import injector from "./injector.js";
 import DOM from "./DOM.js";
 
-console.log(storage.props().location())
-//REMOVE THIS ONCE FINISHED
 
 
 //storage.props().location("christchurch")
+// console.log(storage.props().location())
+//REMOVE THIS ONCE FINISHED
+
+
+
 //console.log(storage.getLocal("localWeatherCopy"))
 
 injector().exec("today");
