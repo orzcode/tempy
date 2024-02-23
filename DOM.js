80f129926292b144fdbb41b372341d89b08c95e7
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-23 17:19:45 +1300

    ~

--- DOM.js
+++ DOM.js
@@ -12,20 +12,6 @@ const DOM = () => {
 		locField.addEventListener("keypress", () => updateLocation())
 	}
 
-// 	// Get the input field
-// var input = document.getElementById("myInput");
-
-// Execute a function when the user presses a key on the keyboard
-// input.addEventListener("keypress", function(event) {
-//   // If the user presses the "Enter" key on the keyboard
-//   if (event.key === "Enter") {
-//     // Cancel the default action, if needed
-//     event.preventDefault();
-//     // Trigger the button element with a click
-//     document.getElementById("myBtn").click();
-//   }
-// });
-
 	const updateLocation = () => {
 		if (event.key === "Enter") {
 			// Form preventDefault
@@ -33,13 +19,9 @@ const DOM = () => {
 			
 			// Do this stuff once Enter is pressed:
 			storage.props().location(locField.value);			
-			injector().exec("today")
-			
+			injector().exec("today")			
 		  }
-
 	}
-
-
 
 	return { applyListeners }
 }
