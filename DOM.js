b53c69333219691dbc80a863044fab991c93a11c
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-29 16:55:03 +1300

    splashscreen MVP

--- DOM.js
+++ DOM.js
@@ -3,27 +3,29 @@ import injector from "./injector.js";
 import storage from "./storage.js";
 
 const DOM = () => {
-	const locField = document.getElementById("location");
+	const locField = document.getElementById("location"); // Main Location field
+	const splashLocField = document.getElementById("splashLocation"); // Splashscreen Location field
 
 	const applyListeners = () => {
 		document.querySelector("#today").addEventListener("click", () => injector().exec("today"));
 		document.querySelector("#tomorrow").addEventListener("click", () => injector().exec("tomorrow"));
 
-		locField.addEventListener("keypress", () => updateLocation())
+		locField.addEventListener("keypress", () => updateLocation(locField));
+		splashLocField.addEventListener("keypress", () => updateLocation(splashLocField));
 	}
 
-	const updateLocation = () => {
+	const updateLocation = (inputField) => {
 		if (event.key === "Enter") {
-			// Form preventDefault
 			event.preventDefault();
-			
-			// Do this stuff once Enter is pressed:
-			storage.props().location(locField.value);			
-			injector().exec("today")			
-		  }
+
+			storage.props().location(inputField.value);
+			injector().exec("today");
+
+			document.querySelector("dialog").close()
+		}
 	}
 
-	return { applyListeners }
+	return { applyListeners };
 }
 
 export default DOM
--- index.js
+++ index.js
@@ -6,8 +6,7 @@ import DOM from "./DOM.js";
 
 //storage.clear()
 
-storage.props().location("christchurch")
- console.log(storage.props().location())
+//storage.props().location("christchurch")
 //REMOVE THIS ONCE FINISHED
 
 //console.log(storage.getLocal("localWeatherCopy"))
@@ -16,9 +15,9 @@ injector().exec("today");
 DOM().applyListeners()
 
 
-const noLocation = () => {
-	const locData = localStorage.getItem(location);
-	if(locData == null){
-		//do stuff
+const noLocation = (() => {
+	const locData = localStorage.getItem("location");
+	if(locData === null){
+		document.querySelector("dialog").show()
 	}
-}
+})()
