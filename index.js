25ad907792d75ba126015ec20665bd578c7ac7e7
orzcode <71000311+orzcode@users.noreply.github.com>
2024-02-29 16:58:32 +1300

    ~

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ DOM().applyListeners()
 
 
 const noLocation = (() => {
+	//runs automatically at pageload
+	//checks to see if a location is set.
+	//if not, shows splashscreen to let user enter location.
 	const locData = localStorage.getItem("location");
 	if(locData === null){
 		document.querySelector("dialog").show()
